d8f5a015e181d37b9651f09c2d4d77f495e83327
piyush19sharma <sruby8804@gmail.com>
2023-11-03 22:19:19 +0530

    Added exception handling

--- practicals/Practical.php
+++ practicals/Practical.php
@@ -7,10 +7,17 @@ class Practical {
     }
 
     public static function add($num1, $num2) {
+        if (!is_int($num1) || !is_int($num2)) {
+            throw new \InvalidArgumentException("Both inputs must be integers.");
+        }
         return $num1 + $num2;
     }
 
     public static function generateFibonacciSequence($n) {
+        if (!is_int($n) || $n < 1) {
+            throw new \InvalidArgumentException("Input must be a positive integer.");
+        }
+
         $fibonacciSequence = [0, 1];
         for ($i = 2; $i < $n; $i++) {
             $fibonacciSequence[$i] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
